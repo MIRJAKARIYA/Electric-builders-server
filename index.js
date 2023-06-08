274e10fad795c25a70bbc294d22d5dd4f2cb446a
MIRJAKARIYA <mirjakariya239@gmail.com>
2023-06-08 20:24:08 +0600

    some changes

--- index.js
+++ index.js
@@ -33,12 +33,19 @@ const verifyToken = (req, res, next) => {
 };
 
 
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gnzth.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 const run = async () => {
   try {
-    await client.connect();
 
     //tools collection
     const toolsCollection = client.db("toolManufacturer").collection("tools");
